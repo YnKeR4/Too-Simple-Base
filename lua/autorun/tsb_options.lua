897f49671e55d30df3595c86ba252409e6633c4c
YnKeR4 <151441222+YnKeR4@users.noreply.github.com>
2023-12-26 12:14:40 +0900

    Update tsb_options.lua

--- lua/autorun/tsb_options.lua
+++ lua/autorun/tsb_options.lua
@@ -7,7 +7,6 @@ hook.Add( "PopulateToolMenu", "TSBMenu", function()
 		panel:CheckBox("Disable: «Jamming»", "tsb_force_disable_jamming")
 		panel:CheckBox("Disable: «Overheating»", "tsb_force_disable_overheating")
 		panel:CheckBox("Force: Can shoot while running", "tsb_force_shoot_while_running")
-		panel:CheckBox("Force: Shoots only in iron", "tsb_force_shoot_only_in_iron")
 		
 		panel:NumSlider("Damage Multiplier (Player)", "tsb_damage_multi", 0.1, 10, 1)
 		panel:NumSlider("Damage Multiplier (NPC)", "tsb_damage_multi_npc", 0.1, 10, 1)
